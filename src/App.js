3aba9ef1d8a480f40999bc47bf24b8e0aab2c5a4
Noni <noni.manzano@gmail.com>
2018-01-30 22:14:21 -0600

    used pseudo selectors

--- src/App.js
+++ src/App.js
@@ -60,20 +60,9 @@ class App extends Component {
   }
 
   render () {
-    const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer',
-      // ':hover': {
-      //   backgroundColor: 'lightgreen',
-      //   color: 'black'
-      // }
-    }
 
     let persons = null;
+    let btnClass = '';
 
     if (this.state.showPersons) {
       persons = (
@@ -90,7 +79,7 @@ class App extends Component {
 
         </div>
       );
-      style.backgroundColor = 'red';
+      btnClass = classes.Red;
     }
 
     const assignedClasses = [];
@@ -105,7 +94,7 @@ class App extends Component {
       <div className={classes.App}>
         <h1>Hi, I'm a React App</h1>
         <p className={assignedClasses.join(' ')}> This is really working! </p>
-      <button style={style} onClick={this.togglePersonsHandler}>Toggle Persons</button>
+      <button className={btnClass} onClick={this.togglePersonsHandler}>Toggle Persons</button>
       {persons}
     </div>
     );
